659134f512c990254e2207c249ace99ebce44a5e
Rob Thijssen <rthijssen@gmail.com>
2021-06-04 10:42:03 +0300

    tidied discord message handler

--- src/channel/discord.ts
+++ src/channel/discord.ts
@@ -29,9 +29,7 @@ export class DiscordChannel extends ChannelBase {
   async start() {
     logger.info(`discord started`);
     await this.client.login(this.config.token);
-
     this.service.registMessageHander(this.channelName, this.sendSuccessMessage);
-    logger.info(`discord message handler registered for ${this.channelName}`);
     this.client.on("message", (msg) => {
       this.messageHandler(msg);
     });
@@ -64,57 +62,41 @@ export class DiscordChannel extends ChannelBase {
 
     const [command, param1] = this.getCommand(msg.content);
 
-    logger.info(command);
-
-    if (channelName !== this.config.activeChannelName && ["!balance", "!drip", "!faucet"].includes(command)) {
-      if (msg.member) {
-        const guildChannels = msg.member.guild.channels.cache;
-        for (const [_, channel] of guildChannels) {
-          if (channel.name === this.config.activeChannelName && channel.type === "text") {
-            const textChannel = channel as Discord.TextChannel;
-            textChannel.send(`${msg.author.toString()} you can try using the \`${command}\` command here!`)
-            
-            break;
+    if (channelName !== this.config.activeChannelName && ["!balance", "!drip", "!faucet"].includes(command) && msg.member) {
+      const guildChannels = msg.member.guild.channels.cache;
+      for (const [_, channel] of guildChannels) {
+        if (channel.name === this.config.activeChannelName && channel.type === "text") {
+          const textChannel = channel as Discord.TextChannel;
+          textChannel.send(`${msg.author.toString()} you can try using the \`${command}\` command here!`)
+          break;
+        }
+      }
+    } else if (channelName === this.config.activeChannelName && ["!balance", "!drip", "!faucet"].includes(command)) {
+      switch (command) {
+        case '!balance':
+          const balances = await this.service.queryBalance();
+          msg.channel.send(this.service.getMessage("balance", { balance: balances.map((item) => `${item.token}: ${item.balance}`).join(", ") }));
+          break;
+        case '!drip':
+          const address = param1;
+          try {
+            await this.service.faucet({
+              strategy: "normal",
+              address: address,
+              channel: {
+                channelId: msg.channel.id,
+                name: this.channelName,
+                account: account,
+                accountName: name,
+              },
+            });
+          } catch (e) {
+            msg.channel.send(e.message ? e.message : this.service.getErrorMessage("COMMON_ERROR", { account }));
           }
-        }
-      }
-    } else if (channelName === this.config.activeChannelName) {
-      if (command === "!faucet") {
-        msg.reply(this.service.usage());
-      }
-
-      if (command === "!balance") {
-        const balances = await this.service.queryBalance();
-        msg.channel.send(
-          this.service.getMessage("balance", {
-            balance: balances
-              .map((item) => `${item.token}: ${item.balance}`)
-              .join(", "),
-          })
-        );
-      }
-
-      if (command === "!drip") {
-        const address = param1;
-
-        try {
-          await this.service.faucet({
-            strategy: "normal",
-            address: address,
-            channel: {
-              channelId: msg.channel.id,
-              name: this.channelName,
-              account: account,
-              accountName: name,
-            },
-          });
-        } catch (e) {
-          msg.channel.send(
-            e.message
-              ? e.message
-              : this.service.getErrorMessage("COMMON_ERROR", { account })
-          );
-        }
+          break;
+        case '!faucet':
+          msg.reply(this.service.usage());
+          break;
       }
     }
   }
